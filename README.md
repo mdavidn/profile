550f69bf6ea306454ea7db2d6e43a4fc1e48c4d8
Matthew Nelson <mnelson@vitalvector.com>
2014-02-03 23:33:33 -0800

    Backup previous files

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ Personal Profile
 
 I grew tired of deciphering the subtle difference between `.profile`,
 `.bash_profile`, and `.bashrc` on every new system, so I created this.
-To install, download and run `install.sh`. **The script will overwite
-existing profile files.** On systems with curl:
+To install, download and run `install.sh`. On systems with curl:
 
 ```sh
 curl -fsSL https://raw.github.com/mdavidn/profile/master/install.sh | sh
--- install.sh
+++ install.sh
@@ -2,20 +2,58 @@
 # Install personal profile.
 # Matthew Nelson <mnelson@vitalvector.com>
 
+dst_dir="${PREFIX:-$HOME}"
+src_dir="$(mktemp -d "$dst_dir/.profile.XXXX")"
+
 warn() {
   echo "$@" 1>&2
 }
 
 download() {
   if command -v curl >/dev/null; then
-    curl -fsSL $1 || exit 1
+    curl -fsSL -- "$1" || exit 1
   elif command -v wget >/dev/null; then
-    wget -nv -O - $1 || exit 1
+    wget -nv -O - -- "$1" || exit 1
   else
     warn 'Not found: curl or wget'
     exit 1
   fi
 }
 
+each_file() {
+  local dir file
+  for dir; do
+    find "$dir" -type f | while read file; do
+      echo "${file#$dir/}"
+    done
+  done
+}
+
+install() {
+  local file src dst backup
+  while read file; do
+    src="$src_dir/$file"
+    dst="$dst_dir/$file"
+    mkdir -p -- "${dst%/*}"
+    if [ -e "$dst" ] && ! cmp -s -- "$src" "$dst"; then
+      backup="$(mktemp "$dst.bak.XXXX")"
+      mv -- "$dst" "$backup"
+      case "$file" in
+        .ssh/authorized_keys)
+          warn "Merging $file"
+          sort -u "$src" "$backup" > "$dst"
+          ;;
+        *)
+          warn "Backing up $file"
+          mv -- "$src" "$dst"
+      esac
+    else
+      mv -- "$src" "$dst"
+    fi
+  done
+}
+
 download https://github.com/mdavidn/profile/archive/master.tar.gz \
-  | tar xzC $HOME --strip-components 2
+  | tar xzC "$src_dir" --strip-components 2
+each_file "$src_dir" | install
+rm -rf "$src_dir"
